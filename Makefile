ff1ae65399837057f0e2341e68701231acafec9c
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-06-10 16:01:09 +0200

    Plots rot und grün

--- Makefile
+++ Makefile
@@ -6,6 +6,11 @@ build/v500.pdf: v500.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.
 	biber build/v500.bcf
 	lualatex  --output-directory=build v500.tex
 
+build/plotrot.pdf:  plotrot.py| build
+	python plotrot.py
+	
+build/plotgruen.pdf:  plotgruen.py| build
+	python plotgruen.py
 
 build: 
 	mkdir -p build
---
+++ plotgruen.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from numpy import sqrt
+import scipy.constants as const
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)  
+
+U1,I1= np.genfromtxt('gruen.txt', unpack= True, skip_header=1, skip_footer=12)
+U2,I2= np.genfromtxt('gruen.txt', unpack= True, skip_header=24)
+U3,I3= np.genfromtxt('gruen2.txt', unpack= True, skip_header=1, skip_footer=0)
+def g(x,a,b):
+    return a*x+b
+
+def null(x):
+    return 0*x
+
+para,pcov =curve_fit(g,U3,sqrt(I3))#,p0=[1,mean,sigma])
+a,b = para
+pcov = np.sqrt(np.diag(pcov))
+fa, fb = pcov
+ua = ufloat(a, fa) 
+ub = ufloat(b, fb)
+
+xx= np.linspace(-0.9,0.7,100)
+xaxis=np.linspace(-2,2,10)
+
+print('ua',ua)
+print('ub',ub)
+plt.plot(-0.8705,0,'xg',linewidth = 2, label = r'U_b', alpha=1)
+plt.plot(xaxis, null(xaxis) ,'black',linewidth = 1, alpha=0.5)
+plt.plot(xx, g(xx, *para), 'orange', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
+plt.plot(U1, sqrt(I1), 'xb', markersize=6 , label = 'Beschleunigung (V>0)', alpha=0.5)
+plt.plot(U2, sqrt(I2), 'xr', markersize=6 , label = 'Bremsung ( V<0)', alpha=0.5)
+plt.xlabel(r'$U \, / \, \mathrm{V}$')
+plt.ylabel(r'$\sqrt{I} \mathrm{/} \mathrm{pA}^{1/2} $')
+plt.legend(loc="best")                  # legend position
+plt.grid(True)                          # grid style
+plt.savefig('build/plotgruen.pdf', bbox_inches = "tight")
+plt.clf() 
