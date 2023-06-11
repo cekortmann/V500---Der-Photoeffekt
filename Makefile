c50d5590286a1f18d869a6ca9c82c461e9ecf4a2
Celina Kortmann <celina.kortmann@tu-dortmund.de>
2023-06-11 10:57:06 +0200

    plots

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
 all: build/v500.pdf
 
-build/v500.pdf: v500.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
+build/v500.pdf: build/plotgruen.pdf build/plotlila.pdf build/plotgelb.pdf build/plotrot.pdf v500.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
 	lualatex  --output-directory=build v500.tex
 	lualatex  --output-directory=build v500.tex
 	biber build/v500.bcf
 	lualatex  --output-directory=build v500.tex
 
-build/plotrot.pdf:  plotrot.py| build
+build/plotrot.pdf:  rot.txt rot2.txt plotrot.py| build
 	python plotrot.py
 	
-build/plotgruen.pdf:  plotgruen.py| build
+build/plotgruen.pdf: gruen.txt gruen2.txt plotgruen.py| build
 	python plotgruen.py
+
+build/plotlila.pdf: lila.txt lila3.txt plotlila.py| build
+	python plotlila.py
+
+build/plotgelb.pdf: gelb.txt plotgelb.py| build
+	python plotgelb.py
 
 build: 
 	mkdir -p build
---
+++ plotgelb.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from numpy import sqrt
+import scipy.constants as const
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)  
+
+U1,I1= np.genfromtxt('gelb.txt', unpack= True, skip_header=1, skip_footer=19)
+U2,I2= np.genfromtxt('gelb.txt', unpack= True, skip_header=23)
+def g(x,a,b):
+    return a*x+b
+
+def null(x):
+    return 0*x
+
+#plt.plot(-1.6649,0,'xg',linewidth = 2, label = r'U_b', alpha=1)
+#plt.plot(xaxis, null(xaxis) ,'black',linewidth = 1, alpha=0.5)
+#plt.plot(xx, g(xx, *para), 'orange', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
+plt.plot(U1, I1, 'xb', markersize=6 , label = 'Beschleunigung (V>0)', alpha=0.5)
+plt.plot(U2, I2, 'xr', markersize=6 , label = 'Bremsung ( V<0)', alpha=0.5)
+plt.xlabel(r'$U \, / \, \mathrm{V}$')
+plt.ylabel(r'$I \mathrm{/} \mathrm{pA} $')
+plt.legend(loc="best")                  # legend position
+plt.grid(True)                          # grid style
+plt.savefig('build/plotgelb.pdf', bbox_inches = "tight")
+plt.clf() 
