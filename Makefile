ef7c10ad9246fe10af9a5d62e39e871846465bab
Celina Kortmann <celina.kortmann@tu-dortmund.de>
2023-06-12 16:20:51 +0200

    Auswertunf fertig

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: build/v500.pdf
 
-build/v500.pdf: build/plotgruen.pdf build/plotlila.pdf build/plotgelb.pdf build/plotrot.pdf v500.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
+build/v500.pdf: build/frequenz.pdf build/plotgruen.pdf build/plotlila.pdf build/plotgelb.pdf build/plotrot.pdf v500.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
 	lualatex  --output-directory=build v500.tex
 	lualatex  --output-directory=build v500.tex
 	biber build/v500.bcf
@@ -8,7 +8,10 @@ build/v500.pdf: build/plotgruen.pdf build/plotlila.pdf build/plotgelb.pdf build/
 
 build/plotrot.pdf:  rot.txt rot2.txt plotrot.py| build
 	python plotrot.py
-	
+
+build/plotfrequenz.pdf:  frequenz.txt frequenz2.txt frequenz.py| build
+	python frequenz.py
+
 build/plotgruen.pdf: gruen.txt gruen2.txt plotgruen.py| build
 	python plotgruen.py
 
---
+++ frequenz.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from numpy import sqrt
+import scipy.constants as const
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)  
+
+lam, nu, Ug= np.genfromtxt('frequenz.txt', unpack= True, skip_header=1)
+lam2, nu2, Ug2= np.genfromtxt('frequenz2.txt', unpack= True, skip_header=1)
+
+def g(x,a,b):
+    return a*x+b
+
+def null(x):
+    return 0*x
+
+para,pcov =curve_fit(g,nu2,Ug2)#,p0=[1,mean,sigma])
+a,b = para
+pcov = np.sqrt(np.diag(pcov))
+fa, fb = pcov
+ua = ufloat(a, fa) 
+ub = ufloat(b, fb)
+
+xx= np.linspace(0,9,1000)
+
+print('ua',ua)
+print('ub',ub)
+
+plt.plot(xx, g(xx, *para), 'orange', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
+plt.plot(nu, Ug, 'xb', markersize=6 , label = 'Grenzspannungen', alpha=0.5)
+plt.xlabel(r'$\nu \, / \, \mathrm{10^{14} Hz}$')
+plt.ylabel(r'$U_{g} \mathrm{/} \mathrm{V} $')
+plt.xlim(0,9)
+plt.legend(loc="best")                  # legend position
+plt.grid(True)                          # grid style
+plt.savefig('build/frequenz.pdf', bbox_inches = "tight")
+plt.clf() 
